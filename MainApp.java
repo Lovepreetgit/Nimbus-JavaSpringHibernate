61af13cbfefd5109ace9c0796305fb9faa4d657c
Lovepreetgit <169080493+Lovepreetgit@users.noreply.github.com>
2025-11-05 14:12:59 +0530

    Create MainApp.java

---
+++ MainApp.java
@@ -0,0 +1,13 @@
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class MainApp {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context =
+            new AnnotationConfigApplicationContext(AppConfig.class);
+
+        Student student = context.getBean(Student.class);
+        student.showDetails();
+
+        context.close();
+    }
+}
