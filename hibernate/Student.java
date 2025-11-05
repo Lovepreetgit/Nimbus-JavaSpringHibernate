7a7d34cf7e355289e971f07fe056688dd4c975af
Lovepreetgit <169080493+Lovepreetgit@users.noreply.github.com>
2025-11-05 14:27:00 +0530

    Create Student.java

---
+++ hibernate/Student.java
@@ -0,0 +1,20 @@
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Student")
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+    @Column(name = "name")
+    private String name;
+
+    public Student() {}
+    public Student(String name) { this.name = name; }
+
+    public int getId() { return id; }
+    public String getName() { return name; }
+    public void setId(int id) { this.id = id; }
+    public void setName(String name) { this.name = name; }
+}
